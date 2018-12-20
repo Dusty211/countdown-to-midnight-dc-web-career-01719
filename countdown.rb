2d5029117a5b7c6fbb27b74c5287e1c314e43d5f
Kyle Houghton <kylebenz@hotmail.com>
2018-12-20 01:22:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,12 @@ def countdown(number)
 "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+"HAPPY NEW YEAR!"
+end
+
 #binding.pry
